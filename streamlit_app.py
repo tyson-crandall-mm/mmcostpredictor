25168bd2b2feec4ef5ff074a4fa6c83916585fb7
tyson-crandall-mm <tcrandall@meadenmoore.com>
2025-06-02 10:10:24 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,20 @@
 import streamlit as st
+import pandas as pd
+import numpy as np
+import seaborn as sns
+import matplotlib.pyplot as plt
+import plotly.express as px
+import gspread
 
-st.title('🎈 App Name')
+st.title('M&M Machine Learning Cost Proposal Tool')
+st.info('This is an Aid, Final Proposals are Subject to Partner Reviews')
 
-st.write('Hello world!')
+# Google Sheet ID and GID
+sheet_id = "1BXiGbxDcj7unwDmS4G3yg72_jORQYJejDYc7S4jEcl4"
+gid = "1289547923"
+
+# Construct export URL
+url = f"https://docs.google.com/spreadsheets/d/{sheet_id}/export?format=csv&gid={gid}"
+
+# Load into DataFrame
+df = pd.read_csv(url)
