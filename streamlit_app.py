d3350cc744b5c920c670ba1333eb2c387d747f1f
tyson-crandall-mm <tcrandall@meadenmoore.com>
2025-06-02 11:31:31 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -69,6 +69,6 @@ with st.sidebar:
   st.write(f"Start Date: {default_start}")
   st.write(f"End Date: {default_finish}")
   # Optional validation
-  if default_start > default_end:
+  if default_start > default_finish:
     st.error("Start date must be before or equal to end date.")
   
