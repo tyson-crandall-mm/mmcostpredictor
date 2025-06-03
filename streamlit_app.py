c3099978e4eb4c0321ac42b1c691c9b32cb0ddc4
tyson-crandall-mm <tcrandall@meadenmoore.com>
2025-06-03 08:44:48 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -180,6 +180,6 @@ if all([project_office, project_state, project_region, client_type, services, st
       if role in user_df.columns:
           new_row[role] = 1
   user_df = pd.concat([user_df, pd.DataFrame([new_row])], ignore_index=True)"""
-  else:
-    st.warning("Please complete all required fields to generate a project summary.")
+else:
+  st.warning("Please complete all required fields to generate a project summary.")
 user_df
